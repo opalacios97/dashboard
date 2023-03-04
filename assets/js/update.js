65156bba11b557c12b9f6586a992371737a6177e
unknown <felixopm1997@gmail.com>
2023-03-04 14:16:11 -0500

    correcion7

--- assets/js/update.js
+++ assets/js/update.js
@@ -1,7 +1,28 @@
 
 let runScripts = () => {
   
-  let chart_bars = document.getElementById("chart-bars")
-  console.log(chart_bars)
+  let chart_bars = document.getElementById("chart-bars");
+  console.log(chart_bars);
+  
+  let icons = document.getElementsByTagName("i");
+  console.log(icons);
+
+  let cards = document.getElementsByClassName("card");
+  console.log(cards);
   
 }
+
+let modifyText = () => {
+  let listOfP = document.getElementsByTagName("p");
+  let elementP = listOfP[3];
+  elementP.innerHTML = "Dinero actual";
+
+  let listOfH4 = document.getElementsByTagName("h4");
+  let elementH4 = listOfH4[0];
+  elementH4.innerHTML = "$301k";
+
+}
+
+
+runScripts()
+modifyText()
